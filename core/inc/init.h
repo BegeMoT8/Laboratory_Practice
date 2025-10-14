79509b4d9d228cc6a56ee7d190a8d7799bfdd4cf
KoTbEgEmOt <iv06mail@gmail.com>
2025-10-14 17:18:01 +0700

    changed buttons ports from GPIOC to GPIOC/D/E/G. Improve code design

--- core/inc/init.h
+++ core/inc/init.h
@@ -4,26 +4,28 @@
 #include "../../CMSIS/Devices/STM32F4xx/Inc/STM32F429ZI/stm32f429xx.h"
 
 void GPIO_Ini(void);
-bool button_bounce_read(uint32_t idr_mask);
-void changeConf(uint32_t PORT0_bit, uint32_t PORT1_bit, uint32_t PORT0_LIGTH, uint32_t PORT1_LIGTH, bool noRepeat);
+bool button_bounce_read(GPIO_TypeDef *GPIOx, uint32_t idr_mask);
+// void changeConf(GPIO_TypeDef *GPIO0, GPIO_TypeDef *GPIO1, uint32_t PORT0_bit, uint32_t PORT1_bit, uint32_t PORT0_LIGTH, uint32_t PORT1_LIGTH, bool noRepeat)
 void button_light_led(GPIO_TypeDef *GPIOx, uint32_t IDR_bit, uint32_t BSRR_BS, uint32_t BSRR_BR);
 void mode_light(uint8_t ratio, uint32_t BSRR_BSx, uint32_t BSRR_BRx);
 void changing_brightness();
 void main_task();
+void changeConf(GPIO_TypeDef *GPIO0, GPIO_TypeDef *GPIO1, uint32_t PORT0_bit, uint32_t PORT1_bit, uint32_t PORT0_LIGTH, uint32_t PORT1_LIGTH, bool noRepeat);
+// NOTE: макросы для настройки режима работы порта PD2
 
-// NOTE: макросы для настройки режима работы порта вручную
+#define GPIOD_BASE_ADDR 0x40020C00UL
+#define GPIOD_MODER_ADDR (GPIOD_BASE_ADDR + 0x00UL)
+#define GPIOD_OTYPER_ADDR (GPIOD_BASE_ADDR + 0x04UL)
+#define GPIOD_OSPEEDR_ADDR (GPIOD_BASE_ADDR + 0x08UL)
+#define GPIOD_PUPDR_ADDR (GPIOD_BASE_ADDR + 0x0CUL)
 
-// RCC + AHB1ENR
-#define RCC_GPIO_EN (*(uint32_t *)(0x40023800UL + 0x30UL))
-// бит в GPIOB
-#define RCC_GPIOB_EN 0x02UL
-//(адрес GPIOB + адрес MODER) для настройки режима порта (00 input; 01 output)
-#define GPIOB_MODER (*(uint32_t *)(0x40020400UL + 0x00UL))
-//(адрес GPIOB + адрес OTYPER) тип выхода (push-pull(0) \ open-Drain(1))
-#define GPIOB_OTYPER (*(uint32_t *)(0x40020400UL + 0x04UL))
-//(адрес GPIOB + адрес OSPEEDR) скорость работы пинов порта
-#define GPIOB_OSPEEDR (*(uint32_t *)(0x40020400UL + 0x08UL))
-// для регистра MODER на output для 0 порта (синий светодиод) |0 бит единичка|
-#define GPIOB_MODE_PIN0_OUT 0x1UL
-// настраиваем тип выхода на push-pull (0)
-#define GPIOB_OTYPER_PIN0_PP 0x0000UL
+#define GPIOD_MODER (*(uint32_t *)(GPIOD_MODER_ADDR))
+#define GPIOD_OTYPER (*(uint32_t *)(GPIOD_OTYPER_ADDR))
+#define GPIOD_OSPEEDR (*(uint32_t *)(GPIOD_OSPEEDR_ADDR))
+#define GPIOD_PUPDR (*(uint32_t *)(GPIOD_PUPDR_ADDR))
+
+// Макросы для конфигурации пина 2 порта D
+#define GPIOD_PIN2_MODER_CLEAR() (GPIOD_MODER &= ~(0x3UL << 4))
+#define GPIOD_PIN2_OTYPER_CLEAR() (GPIOD_OTYPER &= ~(0x1UL << 2))
+#define GPIOD_PIN2_OSPEEDR_MED() (GPIOD_OSPEEDR |= (0x1UL << 4))
+#define GPIOD_PIN2_PUPDR_PULLUP() (GPIOD_PUPDR |= (0x1UL << 4))
--- core/src/init.c
+++ core/src/init.c
@@ -1,49 +1,126 @@
 #include "init.h"
 
+// void GPIO_Ini()
+// {
+//     // NOTE: используемые порты: PC11; PD2; PG2; PE2;
+
+//     // включаем тактирование на пины GPIOC/D/E/G/B
+//     SET_BIT(RCC->AHB1ENR, RCC_AHB1ENR_GPIOCEN | RCC_AHB1ENR_GPIODEN | RCC_AHB1ENR_GPIOEEN | RCC_AHB1ENR_GPIOGEN | RCC_AHB1ENR_GPIOBEN);
+
+//     // кнопки
+
+//     // конфигурируем на вход
+//     CLEAR_BIT(GPIOC->MODER, GPIO_MODER_MODE11);
+//     CLEAR_BIT(GPIOD->MODER, GPIO_MODER_MODE2);
+//     CLEAR_BIT(GPIOG->MODER, GPIO_MODER_MODE2);
+//     CLEAR_BIT(GPIOE->MODER, GPIO_MODER_MODE2);
+
+//     // NOTE: GPIOC 11 всегда работает в режиме входа
+
+//     // настройка типа выхода push pull:
+//     CLEAR_BIT(GPIOD->OTYPER, GPIO_OTYPER_OT_2);
+//     CLEAR_BIT(GPIOG->OTYPER, GPIO_OTYPER_OT_2);
+//     CLEAR_BIT(GPIOE->OTYPER, GPIO_OTYPER_OT_2);
+
+//     // настройка скорости работы - средняя
+//     SET_BIT(GPIOC->OSPEEDR, GPIO_OSPEEDER_OSPEEDR11_0);
+//     SET_BIT(GPIOD->OSPEEDR, GPIO_OSPEEDER_OSPEEDR2_0);
+//     SET_BIT(GPIOG->OSPEEDR, GPIO_OSPEEDER_OSPEEDR2_0);
+//     SET_BIT(GPIOE->OSPEEDR, GPIO_OSPEEDER_OSPEEDR2_0);
+
+//     // подтяжка резисторов - pull up
+//     SET_BIT(GPIOC->PUPDR, GPIO_PUPDR_PUPDR11_0);
+//     SET_BIT(GPIOD->PUPDR, GPIO_PUPDR_PUPDR2_0);
+//     SET_BIT(GPIOG->PUPDR, GPIO_PUPDR_PUPDR2_0);
+//     SET_BIT(GPIOE->PUPDR, GPIO_PUPDR_PUPDR2_0);
+
+//     // светодиоды на плате
+//     CLEAR_BIT(GPIOB->MODER, GPIO_MODER_MODE7 | GPIO_MODER_MODE0 | GPIO_MODER_MODE14);
+//     SET_BIT(GPIOB->MODER, GPIO_MODER_MODE7_0 | GPIO_MODER_MODE0_0 | GPIO_MODER_MODE14_0);
+//     CLEAR_BIT(GPIOB->OTYPER, GPIO_OTYPER_OT7_Msk | GPIO_OTYPER_OT7_Msk | GPIO_OTYPER_OT7_Msk | GPIO_OTYPER_OT7_Msk);
+//     SET_BIT(GPIOB->OSPEEDR, GPIO_OSPEEDER_OSPEEDR7_0 | GPIO_OSPEEDER_OSPEEDR0_0 | GPIO_OSPEEDER_OSPEEDR14_0);
+// }
+
 void GPIO_Ini()
 {
-    // // включаем тактирование на пины GPIOCEN GPIOBEN GPIOAEN
+    // NOTE: используемые порты: PC11; PD2; PG2; PE2;
 
-    // включаем тактирование на пины GPIOB (адрес RCC + адрес RCC_AHB1ENR) |= бит GPIOB
-    RCC_GPIO_EN |= RCC_GPIOB_EN;                   // собственный макрос
-    (*(uint32_t *)(0x40023800UL + 0x30UL)) |= 0x2; // прямое обращение к памяти
-    SET_BIT(RCC->AHB1ENR, RCC_AHB1ENR_GPIOCEN);    // библиотека CMIS
+    // включаем тактирование на пины GPIOC/D/E/G/B
+    SET_BIT(RCC->AHB1ENR, RCC_AHB1ENR_GPIOCEN | RCC_AHB1ENR_GPIODEN | RCC_AHB1ENR_GPIOEEN | RCC_AHB1ENR_GPIOGEN | RCC_AHB1ENR_GPIOBEN);
 
-    // //настройка кнопок
-    // конфигурируем все пины на вход
-    CLEAR_BIT(GPIOC->MODER, GPIO_MODER_MODE8 | GPIO_MODER_MODE9 | GPIO_MODER_MODE10 | GPIO_MODER_MODE11);
-    // тип выхода push pull
-    CLEAR_BIT(GPIOC->OTYPER, GPIO_OTYPER_OT_8 | GPIO_OTYPER_OT_9 | GPIO_OTYPER_OT_10);
-    // скорость работы - средняя
-    SET_BIT(GPIOC->OSPEEDR, GPIO_OSPEEDER_OSPEEDR8_0 | GPIO_OSPEEDER_OSPEEDR9_0 | GPIO_OSPEEDER_OSPEEDR10_0 | GPIO_OSPEEDER_OSPEEDR11_0);
-    // подтяжка резистора pull up
-    SET_BIT(GPIOC->PUPDR, GPIO_PUPDR_PUPDR8_0 | GPIO_PUPDR_PUPDR9_0 | GPIO_PUPDR_PUPDR10_0 | GPIO_PUPDR_PUPDR11_0);
+    // PC11 - настройка через ПРЯМОЕ ОБРАЩЕНИЕ К ПАМЯТИ
+     
+    // Базовый адрес GPIOC: 0x40020800
+    // MODER offset: 0x00, OTYPER: 0x04, OSPEEDR: 0x08, PUPDR: 0x0C
 
-    // Настройка макросом (желтый 0)
-    GPIOB_MODER |= GPIOB_MODE_PIN0_OUT;
-    // GPIOB_OTYPER |= GPIOB_OTYPER_PIN0_PP;
+    // Конфигурируем на вход (MODER = 00 для пина 11, биты 22-23)
+    (*(uint32_t *)(0x40020800UL + 0x00UL)) &= ~(0x3UL << 22);
 
-    // настройка прямым обращением к памяти (красный светодиод 14)
-    //(адрес GPIOB + адрес MODER) для настройки режима порта (00 input; 01 output)
-    (*(uint32_t *)(0x40020400UL + 0x00UL)) |= 0x10000000;
-    // скорость работы порта - средняя
-    (*(uint32_t *)(0x40020400UL + 0x08UL)) |= 0x10000000;
+    // Настройка типа выхода push pull (OTYPER бит 11 = 0)
+    // NOTE: для входа это не критично, но для единообразия сбросим
+    (*(uint32_t *)(0x40020800UL + 0x04UL)) &= ~(0x1UL << 11);
 
-    // настройка CMIS (синий светодиод 7)
-    CLEAR_BIT(GPIOB->MODER, GPIO_MODER_MODE7);
-    SET_BIT(GPIOB->MODER, GPIO_MODER_MODE7_0);
-    CLEAR_BIT(GPIOB->OTYPER, GPIO_OTYPER_OT7_Msk);
-    SET_BIT(GPIOB->OSPEEDR, GPIO_OSPEEDER_OSPEEDR7_0);
+    // Настройка скорости работы - средняя (OSPEEDR = 01 для пина 11, биты 22-23)
+    (*(uint32_t *)(0x40020800UL + 0x08UL)) |= (0x1UL << 22);
+    (*(uint32_t *)(0x40020800UL + 0x08UL)) &= ~(0x2UL << 22);
+
+    // Подтяжка резистора - pull up (PUPDR = 01 для пина 11, биты 22-23)
+    (*(uint32_t *)(0x40020800UL + 0x0CUL)) |= (0x1UL << 22);
+    (*(uint32_t *)(0x40020800UL + 0x0CUL)) &= ~(0x2UL << 22);
+
+     
+    // PD2 - настройка через МАКРОСЫ
+     
+    // Конфигурируем на вход
+    GPIOD_PIN2_MODER_CLEAR();
+
+    // Настройка типа выхода push pull
+    GPIOD_PIN2_OTYPER_CLEAR();
+
+    // Настройка скорости работы - средняя
+    GPIOD_PIN2_OSPEEDR_MED();
+
+    // Подтяжка резистора - pull up
+    GPIOD_PIN2_PUPDR_PULLUP();
+
+     
+    // PG2 и PE2 - настройка через CMSIS (библиотека)
+
+    // Конфигурируем на вход
+    CLEAR_BIT(GPIOG->MODER, GPIO_MODER_MODE2);
+    CLEAR_BIT(GPIOE->MODER, GPIO_MODER_MODE2);
+
+    // Настройка типа выхода push pull
+    CLEAR_BIT(GPIOG->OTYPER, GPIO_OTYPER_OT_2);
+    CLEAR_BIT(GPIOE->OTYPER, GPIO_OTYPER_OT_2);
+
+    // Настройка скорости работы - средняя
+    SET_BIT(GPIOG->OSPEEDR, GPIO_OSPEEDER_OSPEEDR2_0);
+    SET_BIT(GPIOE->OSPEEDR, GPIO_OSPEEDER_OSPEEDR2_0);
+
+    // Подтяжка резисторов - pull up
+    SET_BIT(GPIOG->PUPDR, GPIO_PUPDR_PUPDR2_0);
+    SET_BIT(GPIOE->PUPDR, GPIO_PUPDR_PUPDR2_0);
+
+
+    // Светодиоды на плате (GPIOB) CMSIS
+    CLEAR_BIT(GPIOB->MODER, GPIO_MODER_MODE7 | GPIO_MODER_MODE0 | GPIO_MODER_MODE14);
+    SET_BIT(GPIOB->MODER, GPIO_MODER_MODE7_0 | GPIO_MODER_MODE0_0 | GPIO_MODER_MODE14_0);
+    CLEAR_BIT(GPIOB->OTYPER, GPIO_OTYPER_OT7_Msk | GPIO_OTYPER_OT0_Msk | GPIO_OTYPER_OT14_Msk);
+    SET_BIT(GPIOB->OSPEEDR, GPIO_OSPEEDER_OSPEEDR7_0 | GPIO_OSPEEDER_OSPEEDR0_0 | GPIO_OSPEEDER_OSPEEDR14_0);
 }
 
 // OPTIMIZE: возможно стоит добавить верхнюю границу для обработки зажатой кнопки
-bool button_bounce_read(uint32_t idr_mask)
+
+// принимает аргументом название шины порта и бит для чтения
+// возвращает true если кнопка нажата и else в остальных случаях
+bool button_bounce_read(GPIO_TypeDef *GPIOx, uint32_t idr_mask)
 {
     bool press = false;
     int32_t bounce = 0;
     do
     {
-        if (READ_BIT(GPIOC->IDR, idr_mask) == 0)
+        if (READ_BIT(GPIOx->IDR, idr_mask) == 0)
         {
             bounce++;
             press = true;
@@ -57,16 +134,24 @@ bool button_bounce_read(uint32_t idr_mask)
     return press;
 }
 
-
 // NOTE: принимает аргументом номера битов регистра MODER для установки режима выхода
 //  остальные порты кнопок переходят в режим входа, и биты регистра BSRR для подачи высокого сигнала на кнопки в режиме выход
-void changeConf(uint32_t PORT0_bit, uint32_t PORT1_bit, uint32_t PORT0_LIGTH, uint32_t PORT1_LIGTH, bool noRepeat)
+void changeConf(GPIO_TypeDef *GPIO0, GPIO_TypeDef *GPIO1, uint32_t PORT0_bit, uint32_t PORT1_bit, uint32_t PORT0_LIGTH, uint32_t PORT1_LIGTH, bool noRepeat)
 {
     if (noRepeat == true)
     {
-        CLEAR_BIT(GPIOC->MODER, GPIO_MODER_MODE8 | GPIO_MODER_MODE9 | GPIO_MODER_MODE10);
-        SET_BIT(GPIOC->MODER, PORT0_bit | PORT1_bit);
-        SET_BIT(GPIOC->BSRR, PORT0_LIGTH | PORT1_LIGTH);
+        // сброс
+        CLEAR_BIT(GPIOD->MODER, GPIO_MODER_MODE2);
+        CLEAR_BIT(GPIOG->MODER, GPIO_MODER_MODE2);
+        CLEAR_BIT(GPIOE->MODER, GPIO_MODER_MODE2);
+
+        // смена режима
+        SET_BIT(GPIO0->MODER, PORT0_bit);
+        SET_BIT(GPIO1->MODER, PORT1_bit);
+
+        // свет
+        SET_BIT(GPIO0->BSRR, PORT0_LIGTH);
+        SET_BIT(GPIO1->BSRR, PORT1_LIGTH);
     }
 }
 
@@ -99,11 +184,12 @@ void mode_light(uint8_t ratio, uint32_t BSRR_BSx, uint32_t BSRR_BRx)
     }
 }
 
-// NOTE: дополнительное задание
+//NOTE: дополнительное задание
 void changing_brightness()
 {
+
     static uint8_t bright = 1, counter_press8 = 0, counter_press9 = 0, counter_press10 = 0;
-    if (button_bounce_read(GPIO_IDR_IDR_11) == true)
+    if (button_bounce_read(GPIOC, GPIO_IDR_IDR_11) == true)
     {
         bright++;
     }
@@ -112,18 +198,20 @@ void changing_brightness()
         bright = 1;
     }
 
-    if (button_bounce_read(GPIO_IDR_IDR_8) == true)
+    if (button_bounce_read(GPIOD, GPIO_IDR_IDR_2) == true)
     {
         counter_press8++;
     }
-    if (button_bounce_read(GPIO_IDR_IDR_9) == true)
+    if (button_bounce_read(GPIOG, GPIO_IDR_IDR_2) == true)
     {
         counter_press9++;
     }
-    if (button_bounce_read(GPIO_IDR_IDR_10) == true)
+    if (button_bounce_read(GPIOE, GPIO_IDR_IDR_2) == true)
     {
         counter_press10++;
     }
+
+    
 
     if (counter_press8 % 2 == 1)
     {
@@ -182,14 +270,15 @@ void changing_brightness()
     {
         SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR0_Msk);
     }
+    
 }
 
 // NOTE: основное задание
 void main_task()
 {
-
+    // NOTE: используемые порты: PC11; PD2; PG2; PE2;
     static int8_t counter_press = 0, old_counter_press = 0;
-    if (button_bounce_read(GPIO_IDR_IDR_11) == true)
+    if (button_bounce_read(GPIOC, GPIO_IDR_IDR_11) == true)
     {
         counter_press++;
     }
@@ -198,31 +287,31 @@ void main_task()
 
     if (counter_press == 1)
     {
-        changeConf(GPIO_MODER_MODER8_0, GPIO_MODER_MODER9_0, GPIO_BSRR_BS8, GPIO_BSRR_BS9, (old_counter_press != counter_press));
+        changeConf(GPIOG, GPIOE, GPIO_MODER_MODER2_0, GPIO_MODER_MODER2_0, GPIO_BSRR_BS2, GPIO_BSRR_BS2, (old_counter_press != counter_press));
         old_counter_press = counter_press;
         // 10 на вход, остальные на выход
-        button_light_led(GPIOC, GPIO_IDR_IDR_10, GPIO_BSRR_BS7_Msk, GPIO_BSRR_BR7_Msk);
+        button_light_led(GPIOD, GPIO_IDR_IDR_2, GPIO_BSRR_BS7_Msk, GPIO_BSRR_BR7_Msk);
     }
     else if (counter_press == 2)
     {
-        changeConf(GPIO_MODER_MODER8_0, GPIO_MODER_MODER10_0, GPIO_BSRR_BS8, GPIO_BSRR_BS10, (old_counter_press != counter_press));
+        changeConf(GPIOD, GPIOE, GPIO_MODER_MODER2_0, GPIO_MODER_MODER2_0, GPIO_BSRR_BS2, GPIO_BSRR_BS2, (old_counter_press != counter_press));
         old_counter_press = counter_press;
         // 9 на вход остальные на выход
-        button_light_led(GPIOC, GPIO_IDR_IDR_9, GPIO_BSRR_BS14_Msk, GPIO_BSRR_BR14_Msk);
+        button_light_led(GPIOG, GPIO_IDR_IDR_2, GPIO_BSRR_BS14_Msk, GPIO_BSRR_BR14_Msk);
     }
     else if (counter_press == 3)
     {
-        changeConf(GPIO_MODER_MODER9_0, GPIO_MODER_MODER10_0, GPIO_BSRR_BS9, GPIO_BSRR_BS10, (old_counter_press != counter_press));
+        changeConf(GPIOD, GPIOG, GPIO_MODER_MODER2_0, GPIO_MODER_MODER2_0, GPIO_BSRR_BS2, GPIO_BSRR_BS2, (old_counter_press != counter_press));
         old_counter_press = counter_press;
         // 8 на вход остальные на выход
-        button_light_led(GPIOC, GPIO_IDR_IDR_8, GPIO_BSRR_BS0_Msk, GPIO_BSRR_BR0_Msk);
+        button_light_led(GPIOE, GPIO_IDR_IDR_2, GPIO_BSRR_BS0_Msk, GPIO_BSRR_BR0_Msk);
     }
     else if (counter_press == 4)
     {
         // все порты в режим выхода
         if (old_counter_press != counter_press)
         {
-            SET_BIT(GPIOC->MODER, GPIO_MODER_MODER8_0);
+            SET_BIT(GPIOE->MODER, GPIO_MODER_MODER2_0);
             old_counter_press = counter_press;
         }
     }
@@ -231,12 +320,15 @@ void main_task()
         // все порты в режим входа
         if (old_counter_press != counter_press)
         {
-            CLEAR_BIT(GPIOC->MODER, GPIO_MODER_MODE8 | GPIO_MODER_MODE9 | GPIO_MODER_MODE10);
+            CLEAR_BIT(GPIOD->MODER, GPIO_MODER_MODE2);
+            CLEAR_BIT(GPIOG->MODER, GPIO_MODER_MODE2);
+            CLEAR_BIT(GPIOE->MODER, GPIO_MODER_MODE2);
+
             old_counter_press = counter_press;
         }
-        button_light_led(GPIOC, GPIO_IDR_IDR_10, GPIO_BSRR_BS7_Msk, GPIO_BSRR_BR7_Msk);
-        button_light_led(GPIOC, GPIO_IDR_IDR_9, GPIO_BSRR_BS14_Msk, GPIO_BSRR_BR14_Msk);
-        button_light_led(GPIOC, GPIO_IDR_IDR_8, GPIO_BSRR_BS0_Msk, GPIO_BSRR_BR0_Msk);
+        button_light_led(GPIOD, GPIO_IDR_IDR_2, GPIO_BSRR_BS7_Msk, GPIO_BSRR_BR7_Msk);
+        button_light_led(GPIOG, GPIO_IDR_IDR_2, GPIO_BSRR_BS14_Msk, GPIO_BSRR_BR14_Msk);
+        button_light_led(GPIOE, GPIO_IDR_IDR_2, GPIO_BSRR_BS0_Msk, GPIO_BSRR_BR0_Msk);
     }
     else if (counter_press > 5)
     {
